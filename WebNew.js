bcef871fd8bc81f59586443a1287daa4c0032f99
Yashverma <yv3245650@gmail.comgit config --global user.name YashVerma>
2025-01-17 17:29:12 +0530

    New Think add

---
+++ WebNew.js
@@ -0,0 +1,7 @@
+const card=document.querySelector(".card")
+card.addEventListener("click",()=>{
+    document.querySelector(".card").style.backgroundColor="Red";
+})
+card.addEventListener("dblclick",()=>{
+    document.querySelector(".card").style.backgroundColor="black;"
+})
